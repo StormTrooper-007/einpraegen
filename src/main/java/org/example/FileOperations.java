30871564652d706bd0266503f44d4a3085ed1948
collins <chigbo.c.o@gmail.com>
2024-02-05 21:52:10 +0100

    created method for writing new notes

--- src/main/java/org/example/FileOperations.java
+++ src/main/java/org/example/FileOperations.java
@@ -3,28 +3,40 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileOperations {
     private final Logger logger = LoggerFactory.getLogger(FileOperations.class);
 
-    public void createNewBookDir(String dirName){
+    public void createNewBookDir(Path path, String dirName){
         try {
-            Files.createDirectory(Path.of(dirName));
+            Files.createDirectory(Path.of(String.valueOf(path), dirName));
         }catch(IOException ex){
             logger.info("Could not create directory");
             System.out.println(ex.getMessage());
         }
     }
 
-    public void createNewFile(Path path, String bookName, String fileName){
+    public void createNewFile(Path path, String bookName, String fileName) throws IOException {
         try{
-            Files.createFile(Path.of(path + bookName +"/"+ fileName));
+            Files.createFile(Path.of(String.valueOf(path), bookName, fileName));
         }catch(IOException ex){
-            logger.info("Could not create new file");
+            logger.info("\u001B[34m"+"Could not create new file"+"\u001B[34m");
             System.out.println(ex.getMessage());
+        }
+    }
+    public void writeNotes(Path target) throws IOException{
+        String value = null;
+        try(var in = new BufferedReader(new InputStreamReader(System.in))){
+            do{
+                value = in.readLine();
+            }while(!value.equals("q"));
+
+            Files.writeString(target, value);
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 package org.example;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Path init = new Init().getOperatingSystemDefaultPath();
         FileOperations fileOperations = new FileOperations();
-        fileOperations.createNewBookDir(init + "/" + "newdirectory");
-        fileOperations.createNewFile(init, "/"+"newdirectory", "newfilename.txt");
+        fileOperations.createNewBookDir(init , "newdirectory");
+        fileOperations.createNewFile(init, "newdirectory", "newfilename.txt");
        /* while(true){
             System.out.println("please set path to your files");
              Scanner scanner = new Scanner(System.in);
