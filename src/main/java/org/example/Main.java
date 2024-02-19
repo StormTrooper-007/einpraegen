625f0406197bc30d1dedab41ad129db7f6bab1d6
collins <chigbo.c.o@gmail.com>
2024-02-19 14:31:17 +0100

    created method for writing new notes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         Path init = new Init().getOperatingSystemDefaultPath();
         FileOperations fileOperations = new FileOperations();
